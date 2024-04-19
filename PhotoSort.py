8c1ac3b23930268742d87e0bb1a248e6d51ada0a
Dmitry Konstantinov <rabbitdem@gmail.com>
2024-04-19 13:10:32 +0300

    Добавил строку 12

--- PhotoSort.py
+++ PhotoSort.py
@@ -3,6 +3,10 @@ print('Hello')
 print('Testirovanie')
 print('Stroka 4')
 
+print('Добавить строку 9')
+print('Добавить строку 10')
+print('Добавить строку 11')
+print('Добавить строку 12')
 
 print('test')
 print('Не учитывать эту строку')
