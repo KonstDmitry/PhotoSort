4831e3f5d1c0c8a2c010ec15e6d80929a1a7b08d
Dmitry Konstantinov <rabbitdem@gmail.com>
2024-04-23 19:26:36 +0300

    обновлено

--- file_sort_data.py
+++ file_sort_data.py
@@ -1,1 +1,15 @@
-print('Hello')
+from PIL import Image
+from PIL import ExifTags
+import exiftool
+
+img_open = Image.open('0XT53409.JPG')
+img_data = img_open.getdata()
+img_info = img_open.format
+
+img_exif = ExifTags.Base
+print(img_data)
+print(img_exif)
+
+# a = exiftool.ExifToolHelper.get_tags()
+#
+# print(a)
